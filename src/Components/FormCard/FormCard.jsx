39a910d2aa8f337db448f59fb8cae5376dd2cd50
Gon159x <gonzaxx5@gmail.com>
2023-01-17 22:01:08 -0300

    corrected path

--- src/Components/FormCard/FormCard.jsx
+++ src/Components/FormCard/FormCard.jsx
@@ -20,7 +20,7 @@ export default function FormCard({database, readOnly}) {
         <Container fluid >
             <Row>
                 <h1 className={Style.gradient}>Formulario GreyDive</h1>
-                {showForm ? <p style={readOnly? {display:'none'}:{color:'red',display:'flex',justifyContent:'flex-start'}}>Los campos con * son obligatorios</p> : <p style={{color:'#9f804b',display:'flex',justifyContent:'flex-start'}}>Ruta de sus datos https://greydive-ten.vercel.app/form/{id}.</p> }
+                {showForm ? <p style={readOnly? {display:'none'}:{color:'red',display:'flex',justifyContent:'flex-start'}}>Los campos con * son obligatorios</p> : <p style={{color:'#9f804b',display:'flex',justifyContent:'flex-start'}}>Ruta de sus datos https://greydive-ten.vercel.app/form/{id}</p> }
             </Row>
             <Row style={readOnly? {paddingTop:'32px'}: {} }>
                {showForm ? <DinamycForm readOnly={readOnly} database={database} switchForm={switchForm}/> : <h2 className={Style.gradient}>Felicidades se ha enviado tu formulario correctamente.</h2>}
